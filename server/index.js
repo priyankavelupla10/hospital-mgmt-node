cd503c0243545cb3b2c4712bc5658bde33e61e8c
priyankavelupla10 <119274703+priyankavelupla10@users.noreply.github.com>
2022-12-14 17:26:01 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,23 +1,52 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const http= require('http');
+const fs= require('fs');
+const path = require('path');
+const {MongoClient} = require('mongodb');
+const uri = "mongodb+srv://priya1234:1234@vuemongo.xlr83a0.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
-const app = express();
+const connectDB=async()=>{
+    try{
+        await client.connect();
+        console.log("mongo db is connected")
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+connectDB();
+const server = http.createServer(async(req,res) => {
+const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",  
+  };
+console.log(req.url)
+if(req.url === '/'){
+    fs.readFile( path.join(__dirname,'public','portfolio.html'),(err,data)=>{
 
-app.use(bodyParser.json());
-app.use(cors());
+    if (err) throw err;
+    res.writeHead(200,{ 'Content-Type' : 'text/html'});
+    res.end(data);
+    }
+ )
+ 
+}
+else if(req.url=='/api')
+{
+    const cursor = client.db("vuemongo").collection("hospitals").find({});
+    const results = await cursor.toArray();
+    //console.log(results);
+    const js= (JSON.stringify(results));
+    res.writeHead(200,headers)
+    console.log(js);
+    res.end(js);
 
-const hospitals = require('./routes/api/hospitals');
-
-app.use('/api',hospitals);
-
-app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/portfolio.html'));
-
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(__dirname+'/public/'));
-
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/portfolio.html'));
+}
+else{
+    res.end("Eror 404")
 }
 
-const port = process.env.PORT || 5050;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+});
+
+const PORT = process.env.PORT || 5050;
+server.listen(PORT,() => console.log(`server is running on ${PORT}`));
