febfea0caabab7960f1035c720f2f8a68d72db9c
priyankavelupla10 <119274703+priyankavelupla10@users.noreply.github.com>
2022-12-15 00:40:09 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,52 +1,42 @@
-const http= require('http');
-const fs= require('fs');
-const path = require('path');
-const {MongoClient} = require('mongodb');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const { MongoClient } = require("mongodb");
+
 const uri = "mongodb+srv://priya1234:1234@vuemongo.xlr83a0.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
-const connectDB=async()=>{
-    try{
-        await client.connect();
-        console.log("mongo db is connected")
-    }
-    catch(e){
-        console.log(e)
-    }
-}
+const connectDB = async () => {
+  try {
+    await client.connect();
+  } catch (error) {
+    console.log(error);
+  }
+};
 connectDB();
-const server = http.createServer(async(req,res) => {
-const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",  
-  };
-console.log(req.url)
-if(req.url === '/'){
-    fs.readFile( path.join(__dirname,'public','portfolio.html'),(err,data)=>{
-
-    if (err) throw err;
-    res.writeHead(200,{ 'Content-Type' : 'text/html'});
-    res.end(data);
-    }
- )
- 
-}
-else if(req.url=='/api')
-{
-    const cursor = client.db("vuemongo").collection("hospitals").find({});
-    const results = await cursor.toArray();
-    //console.log(results);
-    const js= (JSON.stringify(results));
-    res.writeHead(200,headers)
-    console.log(js);
-    res.end(js);
-
-}
-else{
-    res.end("Eror 404")
-}
-
+const server = http.createServer(async (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  //res.write("Welcome to Hospitals Management");
+  const url = req.url;
+  if (url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "portfolio.html"),
+      (error, portfolioData) => {
+        //if (error) throw error;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(portfolioData);
+      }
+    );
+  } else if (url == "/api") {
+    const conn = client.db("vuemongo").collection("hospitals").find({});
+    const apiResult = await conn.toArray();
+    const jsonData = JSON.stringify(apiResult);
+    //res.writeHead(200, headers);
+    res.end(jsonData);
+  } else {
+    res.end("Url not found");
+  }
 });
 
-const PORT = process.env.PORT || 5050;
-server.listen(PORT,() => console.log(`server is running on ${PORT}`));
+const PORT = process.env.PORT || 5959;
+server.listen(PORT, () => console.log(`server is running on ${PORT}`));
